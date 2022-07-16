3753e19c22781dd86384bb834983032340ca0f97
Alyxia Sother <lexisoth2005@gmail.com>
2022-07-17 00:05:43 +0200

    [Meta] Initial commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+import {Command} from "commander";
+// import init from "./commands/init.js";
+
+let program = new Command();
+
+program
+    .name("cordwood")
+    .description("A CLI tool for Cordwood plugin devs.")
+    .version("0.0.1");
+
+// program.addCommand(init());
+
+program.parse(process.argv);
